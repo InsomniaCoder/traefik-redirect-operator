089138ca2e485ae31ed392c48973dbb1cff657e9
tanat.l <tanat.l@monix.co.th>
2021-09-21 13:33:10 +0700

    change sample YAML to align with struct

--- controllers/traefikredirect_controller.go
+++ controllers/traefikredirect_controller.go
@@ -18,6 +18,7 @@ package controllers
 
 import (
 	"context"
+
 	traefikv1 "github.com/InsomniaCoder/traefik-redirect-operator/api/v1"
 	"k8s.io/apimachinery/pkg/runtime"
 	ctrl "sigs.k8s.io/controller-runtime"
